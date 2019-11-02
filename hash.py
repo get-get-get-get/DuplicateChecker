b7a340d1080e5e549e45d109a1a695af60ae84d6
4get <getgetgetget@pm.me>
2019-11-01 23:43:47 -0400

    Add sha256sum function

--- hash.py
+++ hash.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
+import hashlib
+
+
+
+def sha256sum(filename):
+
+    h = hashlib.sha256()
+    b = bytearray(128 * 1024)
+    mv = memoryview(b)
+
+    with open(filename, "rb", buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            h.update(mv[:n])
+    
+    return h.hexdigest()
+          
+
 
 
 
 def main():
-    pass
+    print(sha256sum(args.file))
 
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-            "template",
-            help="Put help here"
+        "file",
+        help="file to hash"
     )
 
     args = parser.parse_args()
