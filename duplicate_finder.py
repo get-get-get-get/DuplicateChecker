7bbc19e6c37294618d0bd034417c31e01760230a
4get <getgetgetget@pm.me>
2019-11-03 16:41:15 -0500

    Add delete_duplicates function

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -7,33 +7,14 @@ import queue
 import threading
 
 
-
 class DuplicateFinder:
 
-    max_threads = 10 
+    max_threads = 10
 
     def __init__(self, directory):
         self.directory = str(pathlib.Path(directory))
         self.file_hashes = dict()
         self.duplicates = dict()
-
-    
-    # Add hash as dictionary key
-    def add_hash(self, file_hash, file_path):
-
-        # Lock execution to prevent concurrency mess (bottleneck?)
-        with self._lock:
-            # Confirm then add
-            if not self.file_hashes.get(file_hash, False):
-                self.file_hashes[file_hash] = [file_path]
-            else:
-                self.file_hashes[file_hash].append(file_path)
-    
-
-    # Add file_path to list with hash
-    def add_file(self, file_path, file_hash):
-        self.file_hashes[file_hash].append(file_path)
-    
 
     # Thread worker, managed by hash_files
     def check_files(self, files, hashed):
@@ -43,17 +24,41 @@ class DuplicateFinder:
 
         while not files.empty():
             file = files.get()
-            file_hash = md5sum(file) 
+            file_hash = md5sum(file)
             # Store result
             hf = HashedFile(file_hash, file)
             hashed.append(hf)
 
+    # Deletes all duplicates but the one with shortest path
+    def delete_duplicates(self):
+
+        # Delete duplicates
+        for file_hash, files in self.duplicates.items():
+            # Sort by len
+            files.sort(key=len)
+
+            # Remove file to be kept
+            keep = files.pop(0)
+            print(f"KEPT ==> {keep}")
+            # Delete each redundant item
+            for file in files:
+                os.remove(file)
+                print(f"DELETED ==> {file}")
+            
+            # Update self.file_hashes 
+            self.file_hashes[file_hash] = [keep]
+        
+        # Clear duplicates
+        self.duplicates.clear()
+
+      
 
     # Find duplicates in directory
     # Main function
     def find_duplicates(self):
 
         files = get_files(self.directory)
+        print(f"Found {files.qsize()} files")
 
         # hashed is a list of HashedFile('hash', 'file') tuples
         hashedfiles = self.hash_files(files)
@@ -66,7 +71,6 @@ class DuplicateFinder:
 
         return self.duplicates
 
-
     # Get hashes of all files
     def hash_files(self, files):
 
@@ -76,17 +80,17 @@ class DuplicateFinder:
         workers = []
         for n in range(self.max_threads):
             # Create thread and add to dictionary (hacky...)
-            t = threading.Thread(target=self.check_files, args=(files,hashed_files,))
-            
+            t = threading.Thread(target=self.check_files,
+                                 args=(files, hashed_files,))
+
             # Run thread
             t.start()
             workers.append(t)
-        
+
         for worker in workers:
             worker.join()
-        
+
         return hashed_files
-    
 
     # Given list of namedtuples (HashedFile('hash', 'file')), adds to self.hashes
     def process_hashed_files(self, hashedfiles):
@@ -96,16 +100,15 @@ class DuplicateFinder:
                 self.file_hashes[hf.hash].append(hf.file)
             else:
                 self.file_hashes[hf.hash] = [hf.file]
-    
 
     # Print duplicates in readable format
     def show_duplicates(self):
-        
+
         for file_hash, file_paths in self.duplicates.items():
             print(file_hash)
             for file_path in file_paths:
                 print("\t%s" % file_path)
-    
+
 
 # Make Queue of files in directory (recursive)
 def get_files(directory):
@@ -126,19 +129,21 @@ def md5sum(filename):
     mv = memoryview(b)
 
     with open(filename, "rb", buffering=0) as f:
-        for n in iter(lambda : f.readinto(mv), 0):
+        for n in iter(lambda: f.readinto(mv), 0):
             h.update(mv[:n])
-    
+
     return h.hexdigest()
 
 
 def main():
-    
+
     finder = DuplicateFinder(args.directory)
     finder.max_threads = args.threads
     finder.find_duplicates()
     finder.show_duplicates()
 
+    if args.delete:
+        finder.delete_duplicates()
 
 
 if __name__ == '__main__':
@@ -150,6 +155,11 @@ if __name__ == '__main__':
     parser.add_argument(
         "directory",
         help="file to hash"
+    )
+    parser.add_argument(
+        "--delete",
+        action="store_true",
+        help="Delete redundant files based on path length (keep shortest). Stupid/dangerous"
     )
     parser.add_argument(
         "-t",
